0789aad9ae151ae8d1e08aceaef09bbc9b02a490
magedo992 <ahmeaged900@gmail.com>
2025-04-24 15:56:30 +0200

    task done3

--- index.js
+++ index.js
@@ -5,13 +5,19 @@ const DBconfig=require('./dbconfig/dbconfig');
 require('dotenv').config()
 const port=process.env.port;
 const courseRoute=require('./route/courseRoute');
-app.use(express.json())
+const bodyParser = require('body-parser');
+const cors=require('cors');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use('/api',courseRoute);
 
 
 
-
+app.get('/',(req,res)=>{
+    res.status(200).json({"message":"hello world"});
+})
 
 app.listen(port,()=>{
     DBconfig()
